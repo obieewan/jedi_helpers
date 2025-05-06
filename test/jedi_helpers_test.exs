5d47759ced58bb4c7569228d5dc98304dc332cdf
Obie <elixir.obie@gmail.com>
2025-05-06 15:07:32 +0800

    Add unit tests to changeset helpers and utils

--- test/jedi_helpers_test.exs
+++ test/jedi_helpers_test.exs
@@ -1,8 +1,34 @@
+defmodule JediHelpers.BlogPost do
+  defstruct title: "", content: ""
+end
+
 defmodule JediHelpersTest do
   use ExUnit.Case
-  doctest JediHelpers
+  import JediHelpers
 
-  test "greets the world" do
-    assert JediHelpers.hello() == :world
+  describe "resource_type/1" do
+    test "returns the snake_case module name of a struct" do
+      assert resource_type(%JediHelpers.BlogPost{}) == "blog_post"
+    end
+  end
+
+  describe "uri_parse_path/1" do
+    test "extracts the path from a URI string" do
+      assert uri_parse_path("https://example.com/users/123?ref=home") == "/users/123"
+    end
+
+    test "returns nil when no path is present" do
+      assert uri_parse_path("https://example.com") == nil
+    end
+  end
+
+  describe "format_money/2" do
+    test "formats a valid amount as a currency string" do
+      assert format_money(1200, :php) == "₱1,200.00"
+    end
+
+    test "returns nil if the amount is nil" do
+      assert format_money(nil, :php) == nil
+    end
   end
 end
