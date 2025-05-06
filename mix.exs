5f0cc88dfaa1bcd433ca4534e94c46207f64ac72
Obie <obie.tizon@gmail.com>
2025-05-06 15:10:57 +0800

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule JediHelpers.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description:
+        "Reusable helper functions for Elixir/Phoenix projects with a touch of the Force",
+      package: [
+        name: "jedi_helpers",
+        maintainers: ["Obie"],
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/obieewan/jedi_helpers"}
+      ]
     ]
   end
 
@@ -25,7 +33,8 @@ defmodule JediHelpers.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:ecto, "~> 3.12.0"},
       {:ex_cldr, "~> 2.42.0"},
-      {:ex_money, "~> 5.21.0"}
+      {:ex_money, "~> 5.21.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
