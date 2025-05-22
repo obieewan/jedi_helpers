c6bfc77ecb271868f836b366434f2c9b4130b1bb
Obie <20772607+obieewan@users.noreply.github.com>
2025-05-22 17:34:29 +0800

    Obie/add helper functions (#4)
    * Add trim_description helper functions
    
    * Add format number to words
    
    * Add unique_contraint to trimwhitespace jedihelpers
    
    * Bump version 0.2.2

--- lib/jedi_helpers/changeset_helpers.ex
+++ lib/jedi_helpers/changeset_helpers.ex
@@ -1,37 +1,44 @@
 defmodule JediHelpers.ChangesetHelpers do
+  @moduledoc """
+  Provides helper functions for trimming whitespace and validating string fields
+  in Ecto changesets. Particularly useful for ensuring uniqueness and formatting
+  of string inputs before applying database constraints.
+  """
+
   import Ecto.Changeset
+
+  @field_type :string
 
   @doc """
   Trims leading and trailing whitespaces from the specified fields in the changeset.
   This ensures consistency and helps maintain uniqueness, especially for `citext` fields.
 
-  You can also specify a `max` length in the `opts` to validate that the field's length
-  after trimming does not exceed the specified value. If it exceeds, a validation error is added.
+  ## Options
+
+  - `:max` (`integer`): Maximum allowed length after trimming. If exceeded, a validation error is added. Default is 255.
+  - `:enforce_unique` (`boolean`): When set to `true`, adds a `unique_constraint/3` to the field. Default is `false`.
 
   ## Example
 
-  changeset
-  |> trim_whitespace(:username, max: 50)
-  |> unique_constraint(:username)
+      changeset
+      |> trim_whitespace(:username, max: 50, enforce_unique: true)
 
-  ## Parameters:
-  - `changeset` (`Ecto.Changeset.t()`): The changeset containing the field to be processed.
+  ## Parameters
+
+  - `changeset` (`Ecto.Changeset.t()`): The changeset containing the field(s) to be processed.
   - `field` (`atom()` or `[atom()]`): The field(s) to trim.
-  - `opts` (`keyword()`): Options for the function:
-  - `:max` (`integer()`): The maximum allowed length after trimming. Adds a validation error if exceeded.
+  - `opts` (`keyword()`): Options for trimming and validation.
 
-  ## Returns:
-  - An updated `Ecto.Changeset.t()` with the trimmed field and potential validation error if the length exceeds `max`.
+  ## Returns
+
+  - An updated `Ecto.Changeset.t()` with trimmed values and optional validations.
   """
-
-  @field_type :string
-
   @spec trim_whitespace(Ecto.Changeset.t(), atom() | [atom()], keyword()) :: Ecto.Changeset.t()
-
   def trim_whitespace(changeset, keys, opts \\ [])
 
   def trim_whitespace(changeset, key, opts) when is_atom(key) do
     max = Keyword.get(opts, :max, 255)
+    enforce_unique? = Keyword.get(opts, :enforce_unique, false)
 
     case Map.get(changeset.types, key) do
       type when type == @field_type ->
@@ -44,6 +51,7 @@ defmodule JediHelpers.ChangesetHelpers do
 
             changeset
             |> put_change(key, new_value)
+            |> maybe_enforce_unique(key, enforce_unique?)
             |> validate_length(key, max: max)
 
           _ ->
@@ -56,8 +64,11 @@ defmodule JediHelpers.ChangesetHelpers do
   end
 
   def trim_whitespace(changeset, keys, opts) when is_list(keys) do
-    Enum.reduce(keys, changeset, fn key, changeset ->
-      trim_whitespace(changeset, key, opts)
+    Enum.reduce(keys, changeset, fn key, acc ->
+      trim_whitespace(acc, key, opts)
     end)
   end
+
+  defp maybe_enforce_unique(changeset, key, true), do: unique_constraint(changeset, key)
+  defp maybe_enforce_unique(changeset, _key, false), do: changeset
 end
