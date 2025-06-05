8472f534d096832058f00e55cee6c6385a9aa1ab
Obie <obie.tizon@gmail.com>
2025-06-05 13:13:05 +0800

    Bump 0.2.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JediHelpers.MixProject do
   def project do
     [
       app: :jedi_helpers,
-      version: "0.2.2",
+      version: "0.2.3",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
