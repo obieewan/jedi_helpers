eb3fae0168923f5e190dd49cde8752cf043cb7be
Obie <elixir.obie@gmail.com>
2025-11-19 20:12:44 +0800

    Bump ex_money 5.23

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to this project will be documented in this file.
 
+## [0.2.6] - 2025-11-19
+### Update
+- Bump dependencies
+
 ## [0.2.5] - 2025-07-23
 ### Update
 - Bump dependencies
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule JediHelpers.MixProject do
   def project do
     [
       app: :jedi_helpers,
-      version: "0.2.5",
+      version: "0.2.6",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -39,12 +39,12 @@ defmodule JediHelpers.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:ecto, "~> 3.13.2"},
+      {:ecto, "~> 3.13.5"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:decimal, "~> 2.3.0"},
       {:number, "~> 1.0.5"},
-      {:ex_money, "~> 5.21.0"},
-      {:ex_cldr, "~> 2.42.0"}
+      {:ex_money, "~> 5.23.0"},
+      {:ex_cldr, "~> 2.44.1"}
     ]
   end
 end
